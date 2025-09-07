cfa0c5143f565e64659c362c0b8157f847780b3b
mathswithManish <mathsclass9manishsir@gmail.com>
2025-09-07 09:28:16 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ padding: 30, fontFamily: "system-ui" }}>
+      <h1>Maths with Manish — Site Live ✅</h1>
+      <p>Welcome! Ye tumhara homepage hai. Ab yaha tum apna real content dal sakte ho.</p>
+    </div>
+  );
+}
